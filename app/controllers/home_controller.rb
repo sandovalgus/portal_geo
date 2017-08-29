6696bbae5b6d35e65d0acb145e44ece42cdac451
gus <gustavo.fabian.sandoval@gmail.com>
2017-08-29 19:14:20 -0300

    cruzamiento de zonas y controller destroy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ before_action :authenticate_user!
     @map_hash = Gmaps4rails.build_markers(@addres) do |address, marker|
       marker.lat address.latitude
       marker.lng address.longitude
-      marker.infowindow [address.address,"</br>", address.latitude,address.latitude].join('-')
+      marker.infowindow [address.customer_id, address.address, address.latitude,address.latitude].join('/')
     end
 
 
@@ -20,8 +20,6 @@ before_action :authenticate_user!
          schoolpoints << { :zone => point.zone_id,  :lng => point.longitud, :lat => point.latitud}
       end
     end
-    puts "********schoolpoints***********"
-    puts schoolpoints.inspect
 
     @polyjson = schoolpoints
     @polyjson = @polyjson.to_json
--- app/models/zone.rb
+++ app/models/zone.rb
@@ -1,4 +1,4 @@
 class Zone < ApplicationRecord
-	has_many :coordinate_zones
+	has_many :coordinate_zones,:dependent => :delete_all
 	has_many :customers
 end
